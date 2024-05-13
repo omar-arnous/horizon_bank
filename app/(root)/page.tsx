3f0891e558af03c25cc6ac59e76382fe3a9129cc
omar-arnous <omar99.arnous@gmail.com>
2024-05-13 19:17:58 +0300

    implement transaction history and my banks pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,6 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId });
-  console.log(account);
 
   return (
     <section className="home">
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,6 +1,7 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
@@ -9,7 +10,10 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -25,7 +29,7 @@ const BankCard = ({
             </div>
 
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">{1234}</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -49,6 +53,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   );
 };
